0891047574be639ecb762cebdbd50ed70690f49b
Pratik <cpratik1803@gmail.com>
2021-03-20 15:52:03 +0530

    - Remove the commented code

--- app/src/main/java/com/example/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/MainActivity.kt
@@ -3,20 +3,11 @@ package com.example.jetpackcomposeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.jetpackcomposeplayground.ui.theme.ButtonTypes
 import com.example.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
 
@@ -27,9 +18,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                   // ButtonTypes()
-
+                    ButtonTypes()
                 }
             }
         }
--- app/src/main/java/com/example/jetpackcomposeplayground/ui/theme/component/Button_Types.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/ui/theme/component/Button_Types.kt
@@ -1,36 +1,35 @@
 package com.example.jetpackcomposeplayground.ui.theme
 
-import android.widget.Scroller
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeplayground.ui.theme.reusable_component.HeaderTitle
-import java.util.*
 
 
 @Preview
 @Composable
-fun ButtonTypes_lazy() {
-    LazyColumn(contentPadding = PaddingValues(10.dp),modifier = Modifier.fillMaxSize(),content = {
+fun ButtonTypes() {
+    LazyColumn(contentPadding = PaddingValues(10.dp), modifier = Modifier.fillMaxSize(), content = {
 
         item {
             HeaderTitle(text = "Normal Button")
