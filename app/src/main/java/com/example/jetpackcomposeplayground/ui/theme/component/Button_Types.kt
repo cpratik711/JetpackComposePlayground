9ace42b94aa50e7ee855ba6866e8077bdea436a5
Pratik <cpratik1803@gmail.com>
2021-03-20 16:21:45 +0530

    - Added Outline icon button

--- app/src/main/java/com/example/jetpackcomposeplayground/ui/theme/component/Button_Types.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/ui/theme/component/Button_Types.kt
@@ -2,19 +2,14 @@ package com.example.jetpackcomposeplayground.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -120,6 +115,19 @@ fun ButtonTypes() {
             HeaderTitle(text = "Icon button with tint color")
             IconButton(onClick = { }) {
                 VectorIcon(imageVector = Icons.Default.Favorite, tint = Color.Red)
+            }
+
+
+            HeaderTitle(text = "Outline icon button")
+            OutlinedButton(
+                onClick = { /* ... */ },
+                modifier = Modifier.size(50.dp), // it is important otherwise the button is oval
+                shape = CircleShape,
+                border = BorderStroke(1.dp, Color.Blue),
+                contentPadding = PaddingValues(0.dp),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
+            ) {
+                VectorIcon(Icons.Default.Add)
             }
 
             HeaderTitle(text = "Floating action button")
